4f79b875ee0f8ed3d741193bebc173710a953a39
Alperen Gökbak <swalperen3008@gmail.com>
2023-04-05 00:44:23 +0300

    Added optional statments

--- usingoptional/Main.java
+++ usingoptional/Main.java
@@ -1,32 +1,25 @@
 import java.util.Optional;
 
 public class Main {
-    public static void main(String[] args) {
-        Integer a = null;
-
-        b(Optional.ofNullable(a));
-
-        Optional<String> gender = Optional.of("Male");
-        String name = null;
-
-        c(name);
-
-    }
-    public static void b(Optional<Integer> param){
+    public static void ifPresentExample(Optional<Integer> param){
         param.ifPresent(System.out::println);
     } 
 
-    public static void c(String name){
+    public static void ofNullableExample(String name){
         Optional<String> nameOptional = Optional.ofNullable(name);
         nameOptional.ifPresent(System.out::println);
     }
 
-    public static void printName(String name){
+    public static void ofExample(String name){
         Optional<String> nameOptional = Optional.of(name);
-        try {
-            System.out.println(nameOptional.map(s -> s.toUpperCase()));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        
+        System.out.println(nameOptional.map(s -> s.toUpperCase()).orElse(name));
+    }
+
+    public static void main(String[] args) {
+        String name = "Hello World";
+        String name2 = null;
+
+        ofExample(name2);
     }
 }
